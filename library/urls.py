320830956da2e29169bfac49ac95e36b00122085
Kirk Sudduth <kirksudduth@gmail.com>
2020-08-05 23:47:35 -0500

    got all the details pages showing up

--- library/urls.py
+++ library/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from libraryapp.models import *
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('libraryapp.urls')),
